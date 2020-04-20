3ea1e1a6ced46e343bcda92a6b7319b0b108aa33
DanielleMathey95 <danielle.mathey@yahoo.com>
2020-04-19 19:33:35 -0700

    added card and character components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,41 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cards from "./Cards";
+
+const Characters = props => {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log(response);
+          setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log("Something went wrong!", error)
+      });
+
+  }, [])
+
+  console.log(characters);
+
+  return (
+    <div>
+      {characters.map(character => {
+        return (
+          <Cards 
+            name = {character.name}
+            image = {character.image}
+            origin = {character.origin.name}
+            species = {character.species}
+            type = {character.type}
+        />)
+      })}
+    </div>
+  );
+
+};
+
+export default Characters;
